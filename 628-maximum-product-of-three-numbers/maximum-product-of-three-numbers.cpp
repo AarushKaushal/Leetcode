229d43f71a0ce1b569be517f0e3cf3c3b47c6184
AarushKaushal <79004985+AarushKaushal@users.noreply.github.com>
2026-07-29 16:55:50 +0530

    Time: 15 ms (22.28%) | Memory: 31.5 MB (78.56%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int product=1;
-        if(nums.size() <3 || nums.size() >10000) return 0;
-        for (int i : nums) if(i < -1000 || i>1000) return 0;
         sort(nums.begin(), nums.end());
        int n = nums.size();
 
