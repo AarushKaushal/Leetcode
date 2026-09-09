b3d83c97254c04050cda56d37f3e401ab702317c
AarushKaushal <79004985+AarushKaushal@users.noreply.github.com>
2026-09-09 17:36:19 +0530

    Time: 3 ms (11.59%) | Memory: 28.9 MB (69.35%) - LeetSync

---
+++ 4266-angles-of-a-triangle/angles-of-a-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<double> internalAngles(vector<int>& sides) {
+        sort(sides.begin(), sides.end());
+        int a = sides[0];
+        int b = sides[1];
+        int c = sides[2];
+        if (a + b <= c) return {};
+        double A = acos((b*b + c*c - a*a) / (2.0 * b * c));
+        double B = acos((a*a + c*c - b*b) / (2.0 * a * c));
+        double C = acos((a*a + b*b - c*c) / (2.0 * a * b));
+        A = A * 180.0 / M_PI;
+        B = B * 180.0 / M_PI;
+        C = C * 180.0 / M_PI;
+
+        return {A, B, C};
+    }
+};
